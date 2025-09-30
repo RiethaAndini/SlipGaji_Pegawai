fbed88afa3d31ee956b7d6d8e28b619b12cbc076
Auliarahmahsyahira-rpl1 <auliarahmahsyahira0@gmail.com>
2025-09-30 15:52:04 +0930

    menambahkan cek role

--- app/Http/Middleware/cekRole.php
+++ app/Http/Middleware/cekRole.php
@@ -6,15 +6,14 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class cekRole
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
+class cekRole {
+    // Middleware untuk mengecek peran (role) pengguna sebelum mengakses suatu halaman
+    public function handle(Request $request, Closure $next,  string $role): Response {
+        //Mengecek apakah pengguna sudah login dan memiliki peran yang sesuai
+        if (!$request->user() || $request->user()->role !== $role) {
+            abort(430, 'Unauthorized'); //Jika tidak sesuai, hentikan proses dengan error 403 (Forbidden)
+        }
+        //Jika sesuai, lanjutkan ke request berikutnya
         return $next($request);
     }
 }
